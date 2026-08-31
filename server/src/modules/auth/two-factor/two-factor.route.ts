edef008a05bd7ff2b7dfdce97befa8190bdb0a16
Prithivi <prithivi@users.noreply.github.com>
2026-08-31 14:16:32 +0545

    feat(auth): implement rate limiting for authentication routes

--- server/src/modules/auth/two-factor/two-factor.route.ts
+++ server/src/modules/auth/two-factor/two-factor.route.ts
@@ -4,17 +4,23 @@ import { twoFactorController } from "./two-factor.controller";
 import { Router } from "express";
 import { validateRequest } from "@/middleware/validate-request.middleware";
 import { twoFactorSchema } from "./two-factor.validation";
+import { twoFactorRateLimiter } from "@/middleware/rate-limiting.middleware";
 
 const twoFactorRouter: Router = Router();
 
 twoFactorRouter
   .route("/setup-totp")
-  .post(authenticate, asyncHandler(twoFactorController.setupTotp));
+  .post(
+    authenticate,
+    twoFactorRateLimiter,
+    asyncHandler(twoFactorController.setupTotp),
+  );
 
 twoFactorRouter
   .route("/verify-totp")
   .post(
     authenticate,
+    twoFactorRateLimiter,
     validateRequest({ body: twoFactorSchema }),
     asyncHandler(twoFactorController.verifyAndEnableTotp),
   );
@@ -23,6 +29,7 @@ twoFactorRouter
   .route("/disable-totp")
   .post(
     authenticate,
+    twoFactorRateLimiter,
     validateRequest({ body: twoFactorSchema }),
     asyncHandler(twoFactorController.disableTotp),
   );
@@ -31,6 +38,7 @@ twoFactorRouter
   .route("/regenerate-recovery-codes")
   .post(
     authenticate,
+    twoFactorRateLimiter,
     asyncHandler(twoFactorController.regenerateRecoveryCodes),
   );
 
